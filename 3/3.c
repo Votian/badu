e535b55b832db41bb8b19e5835e351ec5f07d292
ZeroOne <evdeathgun.@gmail.com>
2022-11-19 21:52:30 +0300

    NewNanFile

---
+++ 3/3.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    char x;
+    printf("Write:(only char)  \n");
+
+    scanf("%c",&x);
+
+    printf("You write: %c\n",x);
+    return 0;
+
+}
